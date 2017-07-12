6ac5028d179f9f209a5444a2054ce749d90f73cd
Siam <siam.samad@possible.com>
2017-07-12 01:51:18 -0400

    Updated so that we have nested viewgroups Changed the layout to host the quantity picker to a horizontal format Changed the order message with String variable Concatenated the string with the price Inserted a string on a new line that adds to the string variable

--- app/src/main/java/com/example/siamsamad/justjava/MainActivity.java
+++ app/src/main/java/com/example/siamsamad/justjava/MainActivity.java
@@ -17,53 +17,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Variables declared for us to use later */
+    int quantity = 0;
 
-    int numberOfCoffees = 3;
-
-    /** Tap on the plus button to increase the number displayed
-     *
-     */
-
+    /** Tap on the plus button to increase the number displayed */
     public void increment (View view) {
-        int quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
-
     }
 
-    /** Tap on the minus button to decrease the number displayed
-     *
-     */
-
+    /** Tap on the minus button to decrease the number displayed */
     public void decrement (View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
-
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
+    /** This method is called when the order button is clicked */
 
     public void submitOrder(View view) {
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
-
+        String priceMessage = "Total Due: $" + (quantity * 5);
+        priceMessage = priceMessage + "\nThank you for your order!";
+        displayMessage (priceMessage);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+    /** This method displays the given quantity value on the screen. */
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
+    /** This method displays the given price on the screen. */
+
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 
